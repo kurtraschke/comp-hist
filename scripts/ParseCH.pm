1e2b8f7ae58af3fe1e567c89cf65dfa8df12aa58
sj12fn <sj12fn>
2000-09-13 10:27:07 +0000

    Added Info field and Status field support

--- scripts/ParseCH.pm
+++ scripts/ParseCH.pm
@@ -10,7 +10,7 @@ use vars        qw($VERSION @ISA @EXPORT %EXPORT_TAGS);
 $VERSION = 0.01; #May as well
 
 @ISA = qw(Exporter);
-@EXPORT = qw(&docomment &donode &doname &doref &dotype);
+@EXPORT = qw(&docomment &donode &doname &doref &dotype &dostatus &doinf);
 %EXPORT_TAGS = ();
 
 #non-exported globals (nothing is exported. yet.)
@@ -35,8 +35,8 @@ sub docomment {
 sub donode {
     open FILE, $_[0];
     while(<FILE>){
-	$node[$ncount] = $1 if /^(\S+)/;
-	$ncount++;
+	my $node[$ncount] = $1 if /^(\S+)/;
+	my $ncount++;
     }
     return @node;
 }
@@ -45,7 +45,7 @@ sub doname {
     open FILE, $_[0];
     while(<FILE>){
 	my $node = &$onode;
-	$name{$node} = $1 if /.*Name: (.*)/;
+	my $name{$node} = $1 if /.*Name: (.*)/;
     }
     return %name;
 }
@@ -59,7 +59,7 @@ sub doref {
     while(<FILE>){
 	my $node = &$onode;
 	$ref[$rcnt] = $1 if /.*Reference: (.*)/;
-	$rcnt++;
+	my $rcnt++;
     }
     #Now, get rid of the duplicates
     foreach $item (@ref){
@@ -76,13 +76,48 @@ sub dotype {
     while(<FILE>){
         #This is the whole reason for this module
 	my $node = &$onode;
-        if(/.*Type: hardware/i){ $color{$node} = "color: blue"; }
-        if(/.*Type: OS/i){ $color{$node} = "color: red"; }
-        if(/.*Type: language/i){ $color{$node} = "color: green"; }
-        if(/.*Type: standard/i){ $color{$node} = "color: yellow"; }
-        if(/.*Type: other/i){ $color{$node} = "color: magenta"; } #You don't know about this line. Go away.
-	if(/.*Type: company/i){ $color{$node} = "color: cyan"; }
-        if(/.*Type: announcement/i){$color{$node} = "color: gold"; }
+        if(/.*Type: hardware/i){ my $color{$node} = "color: blue"; }
+        if(/.*Type: OS/i){ my $color{$node} = "color: red"; }
+        if(/.*Type: language/i){ my $color{$node} = "color: green"; }
+        if(/.*Type: standard/i){ my $color{$node} = "color: yellow"; }
+        if(/.*Type: other/i){ my $color{$node} = "color: magenta"; } #You don't know about this line. Go away.
+	if(/.*Type: company/i){ my $color{$node} = "color: cyan"; }
+        if(/.*Type: announcement/i){ my $color{$node} = "color: gold"; }
     }
     return %color;
 }
+
+sub dostatus {
+    open FILE, $_[0];
+    while(<FILE>){
+	my $node = &$onode;
+	if(/.*Status: released/i){ my $shape{$node} = "shape: box"; }
+        if(/.*Status: internal/i){ my $shape{$node} = "shape: triangle"; }
+        if(/.*Status: continual evolution/i){ my $shape{$node} = "shape: ellipse"; }
+        if(/.*Status: prototype/i){ my $shape{$node} = "shape: rhomb"; }
+        if(/.*Status: research/i){ my $shape{$node} = "shape: triangle"; }
+    }
+    return %shape
+    }
+
+sub doinf {
+    open FILE, $_[0];
+    my @inf = ();
+    my $item = '';
+    my %seen = ();
+    my @uinf = ();
+    while(<FILE>){
+        my $node = &$onode;
+        $inf[$icnt] = $1 if /.*Info: (.*)/;
+        my $icnt++;
+    }
+    #Now, get rid of the duplicates
+    foreach $item (@inf){
+        unless ($seen{$item}){
+            $seen{$item} = 1;
+            push(@uinf, $item);
+        }
+    }
+    return @uinf;
+}
+
