339b74ea9c06b0d9850084b249ec0f3df17ab18f
sj12fn <sj12fn>
2000-09-21 15:44:11 +0000

    Internals overhaul

--- scripts/ParseCH.pm
+++ scripts/ParseCH.pm
@@ -4,8 +4,8 @@ require Exporter;
 
 @ISA = qw(Exporter);
 @EXPORT = qw(docomment donode doname dodate doref doinfo dotype dostatus doaka dosucc doctf doinfluence doruns doran dowritten);
-@EXPORT_OK = qw(dofield do1field);
-$VERSION = 1.00;
+@EXPORT_OK = qw(dofield do1field %name %year %month %day %ref %info %type %status @linkfrom @linkto @weight);
+$VERSION = 1.10;
 
 sub docomment {
 	s/#.*//;
@@ -22,75 +22,82 @@ sub do1field {
 }
 
 sub donode {
-    my $node = do1field();
-    return $node;
+    $node = do1field();
 }
 
 sub doname {
-    my $name = dofield(Name);
-    return $name;
+    $name{$node} = dofield(Name);
 }
 
 sub dodate {
     if(/^\b[Date|Founded].*(\d\d\d\d)-((\d\d)?-(\d\d)?)?/){
-	my $year = $1;
-	my $month = $3;
-	my $day = $4;
+	$year{$node} = $1;
+	$month{$node} = $3;
+	$day{$node} = $4;
     }
-    return ($year, $month, $day);
 }
 
 sub doref {
-    my $ref = dofield(Reference);
-    return $ref;
+    $ref{$node} = dofield(Reference);
 }
 
 sub doinfo {
-    my $info = dofield(Info);
-    return $info;
+    $info{$node} = dofield(Info);
 }
 
 sub dotype {
-    my $type = do1field(Type);
-    return $type;
+    $type{$node} = do1field(Type);
 }
 
 sub dostatus {
-    my $status = do1field(Status);
-    return $status;
+    $status{$node} = do1field(Status);
 }
 
 sub doaka {
     my $aka = do1field(Aka);
-    return $aka;
+    push(@linkfrom, $node);
+    push(@linkto, $ran);
+    push(@weight, "runson");
 }
 
 sub dosucc {
     my $succ = do1field("Successor to");
-    return $succ;
+    push(@linkfrom, $node);
+    push(@linkto, $ran);
+    push(@weight, "runson");
 }
 
 sub doctf {
     my $code = do1field("Code taken from");
-    return $code;
+    push(@linkfrom, $node);
+    push(@linkto, $code);
+    push(@weight, "codetaken");
 }
 
 sub doinfluence {
     my $inf = do1field("Influenced by");
-    return $inf;
+    push(@linkfrom, $node);
+    push(@linkto, $inf);
+    push(@weight, "influenced");
 }
 
 sub doruns {
     my $runs = do1field("Runs on");
-    return $runs;
+    push(@linkfrom, $node);
+    push(@linkto, $runs);
+    push(@weight, "runson");
 }
 
 sub doran {
     my $ran = do1field("Ran on");
-    return $ran;
+    push(@linkfrom, $node);
+    push(@linkto, $ran);
+    push(@weight, "runson");
 }
 
 sub dowritten {
     my $written = do1field("Written in");
-    return $written;
+    push(@linkfrom, $node);
+    push(@linkto, $ran);
+    push(@weight, "runson");
 }
